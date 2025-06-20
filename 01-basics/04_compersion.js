a96e121ea667aa7e44ea7f5ddcce677255e5adc3
bhavyamjain0721 <bhavyamjain0926@gmail.com>
2025-06-20 14:59:23 +0530

    Create 04_compersion.js

---
+++ 01-basics/04_compersion.js
@@ -0,0 +1,24 @@
+// *********  basic conversion  *********
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+ 
+// *********  NULL conversion  ********* --------------------------------------------------------------|
+// console.log(null > 0); //false                                                                      |
+// console.log(null==0);  //false                                                                      |
+// console.log(null >= 0); //true  // javascript ne (null)ko convert ker deya 0 me                     |
+//                                                                                                     |-------------[ IS TRHA KE COMPERSION KO AVOID KERNA HAI BECAUSE YE COMFUSING ME DALTE HAI ]
+// // *********  basic conversion  *********                                                           |
+// console.log(undefined == 0);   //false                                                              | 
+// console.log(undefined >0);   //false                                                                | 
+// console.log(undefined <0);   //false  --------------------------------------------------------------|
+
+//            strip chek  (==) //  YE VALUE OR DATATYPE KO CHEKL KERTA HAI 
+
+console.log("2" ===2);
