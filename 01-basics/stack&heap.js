9f07e5db063b4cbefb93154e0adc714ad94d9c34
bhavyamjain0721 <bhavyamjain0926@gmail.com>
2025-06-21 11:17:08 +0530

    Create stack&heap.js

---
+++ 01-basics/stack&heap.js
@@ -0,0 +1,13 @@
+//in language c/c++ is me meomary ko (FREE) kerna or (RESAVE)) kerns MANULLEY kerna padta tha as a programmer
+// THERE ARE TWO TYPES OF DATATYPE /  
+// Primitive /\ non-primitve(reference)
+
+//              THERE ARE TWO TYPES OF MEMORY
+//                             |
+//                          /      \
+//                      [stack]   [heap]
+//                        |     |   |
+//                  (primitive )|( non-primitive )------- types
+//                 is me apna ko| is me apne ko REFERENCE milta hai
+//   copy milta hai variable ka | organil value ka
+//  ex:-number , boolean        |ex:- jo be change ho gya vo oringnal value me hota hai
